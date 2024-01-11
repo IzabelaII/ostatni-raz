9f228c5ebafa9a0653f5dcb164986fe18e1e42eb
IzabelaII <iza.kedzia@gmail.com>
2024-01-11 21:53:52 +0100

    zadanie dnia 4

--- ostatni raz/Program.cs
+++ ostatni raz/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string name = "Ewa";
+char Var = 'W';
+int age = 30;
+
+if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa , lat 30.");
+}
+if (name == "Izabela")
+{
+    Console.WriteLine("NIC");
+}
+
+
+
