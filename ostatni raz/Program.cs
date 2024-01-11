b7da9ff1228d2bb72f69a8b9fcd521ce2e96fd03
IzabelaII <iza.kedzia@gmail.com>
2024-01-11 23:17:14 +0100

    poprawka lekcji 4

--- ostatni raz/Program.cs
+++ ostatni raz/Program.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.Net.NetworkInformation;
 
 string name = "Ewa";
-char Var = 'W';
+bool isWoman = true;
 int age = 30;
 
-if (name == "Ewa" && age == 30)
+if (isWoman == true && age <= 30)
 {
-    Console.WriteLine("Ewa , lat 30.");
+    Console.WriteLine("Kobieta poniżej 30 lat.");
 }
-if (name == "Izabela")
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (!isWoman && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else _ = (age > 30 || !isWoman);
 {
     Console.WriteLine("NIC");
 }
-
-
-
