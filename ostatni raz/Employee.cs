9b94251b5589940980d2500ba9689783c20303a5
IzabelaII <iza.kedzia@gmail.com>
2024-01-19 21:18:18 +0100

    nadal lekcja 6

--- ostatni raz/Employee.cs
+++ ostatni raz/Employee.cs
@@ -5,6 +5,54 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ostatni_raz
-{ 
+{
+    public class Employee
+    {
+        public List<Employee> employees = new List<Employee>();
+        public int maxResult = -1;
+        Employee employeeWithMaxResult = null;
 
+
+        private List<int> score = new List<int>();
+        private string name;
+        private string surname;
+        private int age;
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+        public Employee( string name, string surname, int age)
+        {
+            this.score = score;
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+
+        }
+
+
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+
+        }
+    }
+
+
+
+foreach (int Employee Result in Employees Results)
+{
+    if (Employee Result > employeeWithMaxResult)
+     {
+        employeeWithMaxResult = employee;
+    }
 }
+Console.WriteLine($"Najwyższą oceną pracownika" employeeWithMaxResult "z wielością"maxResult);
--- ostatni raz/Program.cs
+++ ostatni raz/Program.cs
@@ -1,45 +1,52 @@
 ﻿
-
-using ostatni_raz;
-
-Employee employee1 = new Employee("Ada", "1975", "Ada", "Gruszka", 33);
-Employee employee2 = new Employee("Monika", "1975", "Monika", "Jabłko", 25);
-Employee employee3 = new Employee("Zuza", "1975", "Zuza", "Śliwka", 18);
-
-
+Employee employee1 = new Employee("Ada", "Gruszka", 33);
 employee1.AddScore(5);
 employee1.AddScore(2);
 employee1.AddScore(9);
 employee1.AddScore(3);
 employee1.AddScore(3);
 
+Employee employee2 = new Employee("Monika", "Jabłko", 25);
 employee2.AddScore(1);
 employee2.AddScore(1);
 employee2.AddScore(1);
 employee2.AddScore(3);
 employee2.AddScore(3);
 
+Employee employee3 = new Employee("Zuza", "Śliwka", 18);
 employee3.AddScore(3);
 employee3.AddScore(9);
 employee3.AddScore(9);
 employee3.AddScore(6);
 employee3.AddScore(4);
-var result = employee2.Result;
-Console.WriteLine(result);
-Console.WriteLine();
-class Employee
+
+
+
+public class Employee
 {
-    public List<Employee> employees = new List<Employee>();
-    public int maxResult = -1;
-    Employee employeeWithMaxResult = null;
-   
- 
- 
-    private List<int> score = new List<int>();
-    private string name;
-    private string surname;
-    private int age;
-    public class Employees(string login, string password, string name, string surname, int age, int score);
+    List<string> employee = new List<string>();
+
+    public string name;
+    public string surname;
+    public int age;
+    public Employee(string name, string surname, int age)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+    public string Name { get; private set; }
+    public string Surname { get; private set; }
+    public int Age { get; private set; }
+
+    List<int> score = new List<int>();
+    public void AddScore(int number)
+    {
+        this.score.Add(number);
+
+    }
+
+    List<int> result = new List<int>();
     public int Result
     {
         get
@@ -47,35 +54,12 @@ class Employee
             return this.score.Sum();
         }
     }
-    public Employee(string login, string pasword, string name, string surname, int age)
-    {
-        this.score = score;
-        this.Name = name;
-        this.Surname = surname;
-        this.Age = age;
-       
-    }
+   public int maxResult = -1;
+   public Employee employeeWithMaxResult = null;
 
-   
-
-    public string Name { get; private set; }
-    public string Surname { get; private set; }
-    public int Age { get; private set; }
-
-    public void AddScore(int number)
-    {
-        this.score.Add(number);
-
-    }
 }
 
-
-
-foreach (employee in Employees)
+foreach (var employee in Employee)
 {
-    if (Employee  Result > maxResult)
-     {
-        employeeWithMaxResult = employee;
-    }
+    
 }
-Console.WriteLine($"Najwyższą oceną pracownika" employeeWithMaxResult "z wielością" maxResult);
