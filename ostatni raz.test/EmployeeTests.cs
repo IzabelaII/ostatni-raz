f52c250ced2c23017f3438b7d8d0e678f996cfc8
IzabelaII <iza.kedzia@gmail.com>
2024-04-26 19:44:05 +0200

    lekcja 9

---
+++ ostatni raz.test/EmployeeTests.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Diagnostics;
+
+namespace ostatni_raz.test
+{
+    public class EmployeeTests
+    {
+        private Statistics statistics;
+
+        [Test]
+        public void WhenGetAverageValueEmployee()
+        {
+            // arrange
+            var employee = new Employee("Adam", "Nowak");
+            employee.AddGrade(2);
+            employee.AddGrade(2);
+            employee.AddGrade(6);
+            // act
+            statistics = new Statistics();
+            // assert
+            Assert.AreEqual(3, statistics.Average);
+
+        }
+        [Test]
+        public void WhenGetMinValueEmployee()
+        {
+            // arrange
+            var employee = new Employee("Adam", "Nowak");
+            employee.AddGrade(2);
+            employee.AddGrade(2);
+            employee.AddGrade(6);
+            // act
+            statistics = new Statistics();
+            // assert
+            Assert.AreEqual(2, statistics.Min);
+
+        }
+        [Test]
+        public void WhenGetMaxValueEmployee()
+        {
+            // arrange
+            var employee = new Employee("Adam", "Nowak");
+            employee.AddGrade(2);
+            employee.AddGrade(2);
+            employee.AddGrade(6);
+            // act
+            statistics = new Statistics();
+            // assert
+            Assert.AreEqual(6, statistics.Max);
+
+        }
+    }
+}
--- ostatni raz/Program.cs
+++ ostatni raz/Program.cs
@@ -1,54 +1,21 @@
-﻿
+﻿using ostatni_raz;
 
-using ostatni_raz;
+var employee = new Employee("Adam","Nowak");
+employee.AddGrade(2);
+employee.AddGrade(5);
+employee.AddGrade(3);
+var statistics = employee.GetStatistics();
 
-Employee employee1 = new Employee("Adam", "Gruszka", 18);
-Employee employee2 = new Employee("Zuza", "Jabłko", 32);
-Employee employee3 = new Employee("Damian", "Wiśnia", 28);
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics.Min}");
+Console.WriteLine($"Max: {statistics.Max}");
 
-employee1.AddScore(9);
-employee1.AddScore(9);
-employee1.AddScore(9);
-employee1.AddScore(9);
-employee1.AddScore(9);
+//Console.WriteLine("Average:" + statistics.Average);
+//Console.WriteLine("Min:" + statistics.Min);
+//Console.WriteLine("Max:" + statistics.Max);
 
-employee2.AddScore(6);
-employee2.AddScore(3);
-employee2.AddScore(9);
-employee2.AddScore(1);
-employee2.AddScore(9);
-
-employee3.AddScore(7);
-employee3.AddScore(7);
-employee3.AddScore(7);
-employee3.AddScore(7);
-employee3.AddScore(7);
-
-
-List<Employee> employees = new List<Employee>() 
+SetSth(out statistics);
+void SetSth (out Statistics statistics) 
 {
-    employee1,employee2,employee3
-};
-
-var maxResult = -1;
-Employee employeeWithMaxResult = null;
-
-
-
-foreach (var employee in employees)
-{
-    maxResult = Math.Max(employee.Result, maxResult);
-    if (employee.Result == maxResult)
-    {
-        employeeWithMaxResult = employee;
-    }
+    statistics = new Statistics();
 }
-
-Console.WriteLine("Pracownik z największą ilością punktów to:" );
-Console.WriteLine("Imie:" + employeeWithMaxResult.Name+ "");
-Console.WriteLine("Nazwisko:" + employeeWithMaxResult.Surname + "");
-Console.WriteLine("Wiek:" + employeeWithMaxResult.Age + "" +"lat");
-Console.WriteLine("Ilość zdobytych punktów to:");
-Console.WriteLine(maxResult);
-
-
